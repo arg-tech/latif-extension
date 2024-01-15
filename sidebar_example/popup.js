2c295cf86be16bf3f829a58623acae04cf9dc252
ananya <ananyasbhatt@gmail.com>
2024-01-15 22:12:51 +0000

    delete everything i did :')

--- sidebar_example/popup.js
+++
@@ -1,6 +0,0 @@
-// sidebar.js
-const sidebar = document.querySelector('.sidebar');
-
-document.getElementById('toggleSidebar').addEventListener('click', () => {
-  sidebar.classList.toggle('active');
-});
