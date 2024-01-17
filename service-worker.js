07fbf3e2dda8608e5e2953df5d3fddecd5262f5b
Joshua Price <git@jprice.uk>
2024-01-17 22:10:01 +0000

    Open the sidepanel upon clicking the extension

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,7 @@
-//await chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
-
-const GOOGLE_ORIGIN = 'https://www.google.com';
-
-// Attempt to open sidepanel on google.com I think
-chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
-  if (!tab.url) return;
-  const url = new URL(tab.url);
-  // Enables the side panel on google.com
-  if (url.origin === GOOGLE_ORIGIN) {
-    await chrome.sidePanel.setOptions({
-      tabId,
-      path: 'sidepanel.html',
-      enabled: true
-    });
-  } else {
-    // Disables the side panel on all other sites
-    await chrome.sidePanel.setOptions({
-      tabId,
-      enabled: false
-    });
-  }
-});
+// Allows users to open the side panel by clicking on the action toolbar icon
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
 
 // Enable text highlighting
 chrome.action.onClicked.addListener((tab) => {
