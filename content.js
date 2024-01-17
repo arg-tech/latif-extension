94243f3f312fab65f22b7c347f17f81ca159b3c9
Joshua Price <git@jprice.uk>
2024-01-17 12:44:32 +0000

    Make highlight class name unique
    This will reduce the chance for conflict with webpages.

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ document.addEventListener('mouseup', function() {
 
     const range = selection.getRangeAt(0);
     const highlightSpan = document.createElement('span');
-    highlightSpan.classList.add('highlight');
+    highlightSpan.classList.add('highlight-f44eb599-7a93-4deb-8f0f-0f899d72cf97');
     range.surroundContents(highlightSpan);
     selection.removeAllRanges();
 });
