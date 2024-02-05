770046adc84e5e20018f2351b7bb2460cdd1aafd
Joshua Price <git@jprice.uk>
2024-02-05 00:15:45 +0000

    Comment out our special highlight function
    Due to both bugs, and it turns out we don't need it for the dropping
    behaviour.

--- content.js
+++ content.js
@@ -1,21 +1,21 @@
-document.addEventListener('mouseup', function() {
-    const selection = window.getSelection();
-    if (!selection.rangeCount || selection.toString().trim() === '') return;
+// document.addEventListener('mouseup', function() {
+//     const selection = window.getSelection();
+//     if (!selection.rangeCount || selection.toString().trim() === '') return;
 
-    const range = selection.getRangeAt(0);
-    const highlightSpan = document.createElement('span');
-    highlightSpan.classList.add('highlight-f44eb599-7a93-4deb-8f0f-0f899d72cf97');
-    highlightSpan.setAttribute('draggable', true)
+//     const range = selection.getRangeAt(0);
+//     const highlightSpan = document.createElement('span');
+//     highlightSpan.classList.add('highlight-f44eb599-7a93-4deb-8f0f-0f899d72cf97');
+//     highlightSpan.setAttribute('draggable', true)
 
-    const highlightedText = selection.toString();
+//     const highlightedText = selection.toString();
 
-    highlightSpan.addEventListener('dragstart', function (event) {
-        event.dataTransfer.setData('text/plain', highlightedText);
-    });
+//     highlightSpan.addEventListener('dragstart', function (event) {
+//         event.dataTransfer.setData('text/plain', highlightedText);
+//     });
 
-    range.surroundContents(highlightSpan);
-    selection.removeAllRanges();
-});
+//     range.surroundContents(highlightSpan);
+//     selection.removeAllRanges();
+// });
 
 // select the item element
 const item = document.querySelector('.item');
