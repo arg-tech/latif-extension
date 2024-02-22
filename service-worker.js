ab910798d06747af8d15613be9b7c8beaa7f0a1f
Joshua Price <git@jprice.uk>
2024-02-22 18:29:05 +0000

    Remove content-scripts entirely
    As it stands, we're not using this in the extension, though there's a
    decent chance this may change in the future.

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,3 @@
 chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error) => console.error(error));
-
-// Enable text highlighting
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['content.js']
-  });
-});
