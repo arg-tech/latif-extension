5c908e16540af8a201e5f83474b7b07eb5ac739d
Joshua Price <git@jprice.uk>
2024-02-22 22:14:04 +0000

    Change service worker to use async

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,9 @@
 // Allows users to open the side panel by clicking on the action toolbar icon
-chrome.sidePanel
-  .setPanelBehavior({ openPanelOnActionClick: true })
-  .catch((error) => console.error(error));
+// Uses async IIFE, to use top-level await without using modules.
+(async () => {
+  try {
+    await chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+  } catch (error) {
+    console.error('Error setting panel behavior:', error);
+  }
+})();
