bf3459e944a9b673d4d1108ce204da71a599cf4d
Joshua Price <git@jprice.uk>
2024-03-19 16:33:12 +0000

    Format files using prettier

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,9 @@
 // Allows users to open the side panel by clicking on the action toolbar icon
 // Uses async IIFE, to use top-level await without using modules.
-(async () => {
+;(async () => {
   try {
-    await chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+    await chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true })
   } catch (error) {
-    console.error('Error setting panel behavior:', error);
+    console.error('Error setting panel behavior:', error)
   }
-})();
+})()
