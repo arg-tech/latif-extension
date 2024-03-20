5f9cb6c24f3ea450f3dc9213cd406ba4cb59d9ce
Joshua Price <git@jprice.uk>
2024-03-20 14:57:32 +0000

    Update vite build
    This will be required for future commits to build properly. It just adds
    content.js and service-worker.js to the build process and then deposits
    them in the root of dist/. This is required for the extension, but
    wouldn't be for a normal webpage which is why I'm having to do this by
    hand.

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,29 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        content: 'src/content.js',
+        'service-worker': 'src/service-worker.js'
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          if (chunkInfo.name !== 'index') {
+            return chunkInfo.name + '.js'
+          }
+          return 'assets/[name].[hash].js'
+        },
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
     }
   }
 })
