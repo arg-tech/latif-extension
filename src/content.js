7acbd082d1e691d69f5e151d585b958ea41736a5
Joshua Price <git@jprice.uk>
2024-04-08 14:00:33 +0100

    Cleanup scraped webpage a bit
    Remove links from the scraped webpage.

--- src/content.js
+++ src/content.js
@@ -4,6 +4,14 @@ import { convert } from 'html-to-text'
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'getArticleText') {
     const article = new Readability(document.cloneNode(true), { charThreshold: 100 }).parse()
-    sendResponse({ text: convert(article.content) })
+    sendResponse({
+      text: convert(article.content, {
+        selectors: [
+          // Remove links from showing up in mined webpage.
+          { selector: 'img', format: 'skip' },
+          { selector: 'a', format: 'anchor', options: { ignoreHref: 'true' } }
+        ]
+      })
+    })
   }
 })
