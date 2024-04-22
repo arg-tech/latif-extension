0e568b50a52d9403ef00ddddd42b395044dabc30
Joshua Price <git@jprice.uk>
2024-04-22 02:05:57 +0100

    Change analyze table to vue code

--- src/App.vue
+++ src/App.vue
@@ -34,18 +34,38 @@ import PageFooter from './components/PageFooter.vue'
             </th>
           </tr>
         </thead>
-        <tbody>
+        <tbody v-if="!responses.analyze">
           <tr v-for="(evidence, index) in evidences" :key="index">
             <td>
               {{ evidence }}
             </td>
-            <td v-for="(_, index) in get_claims.output.hypothesis" :key="index"></td>
+            <td v-for="(_, index2) in responses.get_claims.output.hypothesis" :key="index2"></td>
+          </tr>
+        </tbody>
+        <tbody v-else>
+          <tr v-for="(evidence, index) in evidences" :key="index">
+            <td>
+              {{ evidence }}
+            </td>
+            <td
+              :style="{ backgroundColor: score }"
+              v-for="(score, index2) in coloredMatrix"
+              :key="index2"
+            ></td>
           </tr>
         </tbody>
       </table>
     </div>
-    <div class="d-grid gap-2">
-      <button type="button" id="analyzeButton" class="btn btn-primary">Analyze ACH Table</button>
+
+    <div v-if="evidences.length !== 0" class="d-grid gap-2">
+      <button @click="analyze" type="button" :disabled="loading.analyze" class="btn btn-primary">
+        Analyze ACH Table
+        <span
+          v-if="loading.analyze"
+          aria-hidden="true"
+          class="spinner-border spinner-border-sm ms-2"
+        ></span>
+      </button>
     </div>
 
     <div class="d-grid gap-2 mt-3">
@@ -104,6 +124,37 @@ export default {
     }
   },
 
+  computed: {
+    coloredMatrix() {
+      return this.responses.analyze.output.full_scoring_matrix.map((t) =>
+        t.map((u) => {
+          let cellColor = u
+          let red, green
+          if (cellColor === -1000) {
+            red = 255
+            green = 0
+          } else if (cellColor < 0) {
+            red = 255
+            green = cellColor * -1 * 255
+          } else if (cellColor == 0) {
+            red = 255
+            green = 255
+          } else {
+            green = 255
+            red = (1 - cellColor) * 255
+          }
+
+          red = Math.round(red)
+          green = Math.round(green)
+
+          // Change bg colour of cell with colour calculated earlier.
+          const toHex = (c) => c.toString(16).padStart(2, '0')
+          return `#${toHex(red)}${toHex(green)}${toHex(0)}`
+        })
+      )
+    }
+  },
+
   methods: {
     async minePage() {
       // Add the loading spinner.
@@ -148,6 +199,45 @@ export default {
     tableDrop(event) {
       let data = event.dataTransfer.getData('text/plain')
       this.evidences.push(data)
+    },
+
+    async analyze() {
+      // Add the loading spinner.
+      this.loading.analyze = true
+
+      // Fetch page data from the API.
+      this.responses.analyze = await fetch('http://178.79.182.88:8080/analyze/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          hypothesis: this.responses.get_claims.output.hypothesis,
+          manual_evidences: this.evidences,
+          max_alignment_limit: -1,
+          min_alignment_limit: -1,
+          hypothesis_nodes: this.responses.get_claims.output.hypothesis_nodes,
+          structure_hypothesis_graph: this.responses.get_claims.output.structure_hypothesis_graph
+        })
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok')
+          }
+          return response.json()
+        })
+        .then((data) => {
+          return data
+        })
+        .catch((error) => {
+          console.error('There has been a problem with your fetch operation:', error)
+        })
+
+      // Log the response to help with debugging.
+      console.log('Analyze: ', this.responses.analyze.output)
+
+      // Remove the loading spinner.
+      this.loading.analyze = false
     }
   }
 }
