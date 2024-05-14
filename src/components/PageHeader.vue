d13a798c69643fb065854a2e60b4aab947918913
Joshua Price <git@jprice.uk>
2024-05-14 18:39:43 +0100

    Create new PageHeader component

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="d-flex">
+    <h1 class="me-auto">Impartial Fact Checker</h1>
+    <a href="https://www.arg.tech/" target="_blank"
+      ><img
+        class="img-fluid me-2"
+        src="/icon128.png"
+        alt="ARG-tech logo"
+        style="width: 40px; height: 40px"
+    /></a>
+  </header>
+</template>
