904abeb21eb632e03f3185472d15aa218ff080bc
Joshua Price <git@jprice.uk>
2024-05-14 18:53:53 +0100

    Remove style tags
    I believe this is all just stuff I forgot to remove when starting this
    vue project.

--- src/App.vue
+++ src/App.vue
@@ -115,34 +115,7 @@ import TableHeader from './components/TableHeader.vue'
   <PageFooter />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
 
 <script>
 export default {
