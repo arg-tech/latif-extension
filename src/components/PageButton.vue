3fe6dbfee176eca6953651068933e54f2ceb080d
Joshua Price <git@jprice.uk>
2024-05-30 04:59:32 +0100

    Move PageButton.vue to composition api

--- src/components/PageButton.vue
+++ src/components/PageButton.vue
@@ -1,16 +1,10 @@
+<script setup>
+defineProps({ loading: Boolean })
+</script>
+
 <template>
   <button type="button" :disabled="loading" class="btn btn-primary">
     <slot />
     <span v-if="loading" aria-hidden="true" class="spinner-border spinner-border-sm ms-2"></span>
   </button>
 </template>
-
-<script>
-export default {
-  props: {
-    loading: {
-        type: Boolean
-    }
-  }
-}
-</script>
