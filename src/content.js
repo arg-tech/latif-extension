ccb79b966d42cca0bc14ed3447193f10f3f6f8af
Joshua Price <git@jprice.uk>
2024-06-03 19:20:46 +0100

    Add getFragmentUrl to content.js
    This will get fragment URL of the selected text on the webpage.

--- src/content.js
+++ src/content.js
@@ -1,5 +1,6 @@
 import { Readability } from '@mozilla/readability'
 import { convert } from 'html-to-text'
+import { generateFragment } from 'text-fragments-polyfill/dist/fragment-generation-utils.js'
 
 function getArticleText() {
   const article = new Readability(document.cloneNode(true), { charThreshold: 100 }).parse()
@@ -14,8 +15,24 @@ function getArticleText() {
   }
 }
 
+function getFragmentUrl() {
+  const result = generateFragment(window.getSelection())
+  if (result.status === 0) {
+    let url = `${location.origin}${location.pathname}${location.search}`
+    const fragment = result.fragment
+    const prefix = fragment.prefix ? `${encodeURIComponent(fragment.prefix)}-,` : ''
+    const suffix = fragment.suffix ? `,-${encodeURIComponent(fragment.suffix)}` : ''
+    const start = encodeURIComponent(fragment.textStart)
+    const end = fragment.textEnd ? `,${encodeURIComponent(fragment.textEnd)}` : ''
+    url += `#:~:text=${prefix}${start}${end}${suffix}`
+    return url
+  }
+}
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'getArticleText') {
     sendResponse(getArticleText())
+  } else if (message.action === 'getFragmentUrl') {
+    sendResponse({ url: getFragmentUrl() })
   }
 })
