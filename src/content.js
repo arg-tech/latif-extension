d9cba3a0609a3596620989b60642860571157c32
Joshua Price <git@jprice.uk>
2024-06-03 19:17:43 +0100

    Refactor content.js to move getArticleText
    This just moves getArticleText into a function so that we can add more
    functions in future without cluttering up the file.

--- src/content.js
+++ src/content.js
@@ -1,17 +1,21 @@
 import { Readability } from '@mozilla/readability'
 import { convert } from 'html-to-text'
 
+function getArticleText() {
+  const article = new Readability(document.cloneNode(true), { charThreshold: 100 }).parse()
+  return {
+    text: convert(article.content, {
+      selectors: [
+        // Remove links from showing up in mined webpage.
+        { selector: 'img', format: 'skip' },
+        { selector: 'a', format: 'anchor', options: { ignoreHref: 'true' } }
+      ]
+    })
+  }
+}
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'getArticleText') {
-    const article = new Readability(document.cloneNode(true), { charThreshold: 100 }).parse()
-    sendResponse({
-      text: convert(article.content, {
-        selectors: [
-          // Remove links from showing up in mined webpage.
-          { selector: 'img', format: 'skip' },
-          { selector: 'a', format: 'anchor', options: { ignoreHref: 'true' } }
-        ]
-      })
-    })
+    sendResponse(getArticleText())
   }
 })
