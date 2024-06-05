aa52ea67072dec58e141a549c7e1a6e1005de3f8
Joshua Price <git@jprice.uk>
2024-06-05 17:04:13 +0100

    Add EvidenceCell.vue
    EvidenceCell.vue contains all the logic required for showing the
    evidence in the table, including the new logic that allows the evidence
    to be clicked on.
    
    This also changes AchTable.vue to use the new EvidenceCell component

--- src/components/AchTable.vue
+++ src/components/AchTable.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { inject } from 'vue'
-import EvidenceTuner from '@/components/EvidenceTuner.vue'
+import EvidenceCell from '@/components/EvidenceCell.vue'
 import TableHeader from '@/components/TableHeader.vue'
 
 const evidenceTunerCellRef = inject('evidenceTunerCellRef')
@@ -59,14 +59,7 @@ function getBackgroundColor(score) {
     </thead>
     <tbody>
       <tr v-for="(evidence, rowIndex) in evidences" :key="rowIndex">
-        <td>
-          <a target="_blank" rel="noreferrer" :href="evidence.url" class="d-block text-decoration-none text-reset">{{
-            evidence.text
-          }}</a>
-          <div v-if="evidenceTunerCellRef && evidenceTunerCellRef[0] === rowIndex" class="mt-3">
-            <EvidenceTuner />
-          </div>
-        </td>
+        <EvidenceCell :evidence :rowIndex />
         <template v-if="!responses.analyze">
           <td v-for="(_, colIndex) in responses.get_claims.output.hypothesis" :key="colIndex"></td>
         </template>
