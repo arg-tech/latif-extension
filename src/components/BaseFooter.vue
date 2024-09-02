69f5f126989148f04609606294adf651272ba43c
Joshua Price <git@jprice.uk>
2024-09-02 23:05:45 +0100

    Reformat footer

--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -8,34 +8,55 @@ const version = manifest.version
 </script>
 
 <template>
-  <footer class="d-flex flex-wrap justify-content-between align-items-center mt-2 border-top">
-    <div class="col-md-4 d-flex align-items-center">
-      <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1"></a>
-      <!-- using target value as blank so that the link opens in a new tab -->
-      <span class="mb-3 mb-md-0 mt-3 text-body-secondary">
-        © 2024 ARG-tech<br />
-        <a href="https://latifproject.eu/" target="_blank">LATIF Project</a>
-      </span>
+  <footer class="container-fluid mt-4 py-3 bg-body-tertiary rounded-top-4">
+    <div class="row mb-2">
+      <div class="col text-start">
+        <!-- using target value as blank so that the link opens in a new tab -->
+        <a
+          class="link-underline link-underline-opacity-0 link-secondary"
+          href="https://latifproject.eu/"
+          target="_blank"
+          >LATIF Project</a
+        >
+      </div>
+
+      <div class="col">
+        <ul class="justify-content-end list-inline hstack gap-3 mb-0">
+          <li>
+            <a
+              class="text-body-secondary"
+              href="https://www.instagram.com/ARG_tech/"
+              target="_blank"
+            >
+              <InstagramIcon />
+            </a>
+          </li>
+          <li>
+            <a
+              class="text-body-secondary"
+              href="https://www.facebook.com/ARGtechOrg/"
+              target="_blank"
+            >
+              <FacebookIcon />
+            </a>
+          </li>
+          <li>
+            <a class="text-body-secondary" href="https://twitter.com/arg_tech" target="_blank">
+              <TwitterIcon />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
 
-    <span>Version: {{ version }}</span>
+    <div class="row text-body-secondary" style="font-size: 0.8rem">
+      <div class="col-6">
+        <span class=""> © 2024 ARG-tech<br /> </span>
+      </div>
 
-    <ul class="nav col-md-4 me-4 justify-content-end list-unstyled d-flex">
-      <li class="ms-3">
-        <a class="text-body-secondary" href="https://www.instagram.com/ARG_tech/" target="_blank">
-          <InstagramIcon />
-        </a>
-      </li>
-      <li class="ms-3">
-        <a class="text-body-secondary" href="https://www.facebook.com/ARGtechOrg/" target="_blank">
-          <FacebookIcon />
-        </a>
-      </li>
-      <li class="ms-3">
-        <a class="text-body-secondary" href="https://twitter.com/arg_tech" target="_blank">
-          <TwitterIcon />
-        </a>
-      </li>
-    </ul>
+      <div class="col-6 text-end">
+        <span>Version: {{ version }}</span>
+      </div>
+    </div>
   </footer>
 </template>
