83d8247b06f37ca14421aa5ae675223ec6a598aa
Joshua Price <git@jprice.uk>
2024-09-24 06:34:38 +0100

    Add add evidence button to ach matrix

---
+++ src/components/AchTableAddEvidence.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue'
+import BaseModal from '@/components/BaseModal.vue'
+import ClaimEditor from '@/components/AchTableClaimEditor.vue'
+import { useStore } from '@/store'
+
+const store = useStore()
+
+const isActive = ref(false)
+const modal = ref(null)
+const claimEditor = ref(null)
+
+function addEvidence(evidence) {
+  store.addEvidence(evidence)
+  modal.value.hide()
+}
+</script>
+
+<template>
+  <div class="d-grid gap-2">
+    <button
+      @click="isActive = true"
+      type="button"
+      class="btn btn-outline-success fw-bold btn-sm text-nowrap"
+    >
+      Add Evidence
+    </button>
+
+    <Teleport to="body">
+      <BaseModal
+        ref="modal"
+        v-on="{
+          'hidden.bs.modal': () => (isActive = false),
+          'shown.bs.modal': () => claimEditor.focus()
+        }"
+        @confirm="() => claimEditor.confirmClaim()"
+        v-if="isActive"
+        title="Add Evidence"
+      >
+        <ClaimEditor ref="claimEditor" @claim="addEvidence" />
+      </BaseModal>
+    </Teleport>
+  </div>
+</template>
