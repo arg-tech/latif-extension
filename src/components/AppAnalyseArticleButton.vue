f53f1a6c28cfd9c7d7ed88b79885648b04397620
ananya <ananyasbhatt@gmail.com>
2024-10-10 01:00:04 +0100

    modify survey
    change low, medium, high to strongly disagree, neither agree nor disagree and strongly agree. also added a text box for the 'other' option

--- src/components/AppAnalyseArticleButton.vue
+++ src/components/AppAnalyseArticleButton.vue
@@ -41,51 +41,53 @@ function analyseThisNewsArticle() {
         title="Why are you selecting this news article?"
         confirmButtonText="Continue"
       >
-        <label for="option1" class="form-label fw-semibold"> Reach of the claim </label>
+        <label for="option1" class="form-label fw-semibold"> Popularity </label>
         <input type="range" class="form-range" id="customRange1" />
         <div class="d-flex justify-content-between">
-          <p class="fs-6">Low</p>
-          <p>Medium</p>
-          <p class="fs-6">High</p>
+          <p class="small-text text-start">Strongly disagree</p>
+          <p class="small-text text-center">Neither disagree nor agree</p>
+          <p class="small-text text-end">Strongly agree</p>
         </div>
-        <label for="option2" class="form-label fw-semibold"> Importance of the claim </label>
+        <label for="option2" class="form-label fw-semibold"> Relevance </label>
         <input type="range" class="form-range" id="customRange1" />
         <div class="d-flex justify-content-between">
-          <p class="fs-6">Low</p>
-          <p>Medium</p>
-          <p class="fs-6">High</p>
+          <p class="small-text text-start">Strongly disagree</p>
+          <p class="small-text text-center">Neither disagree nor agree</p>
+          <p class="small-text text-end">Strongly agree</p>
         </div>
         <label for="option3" class="form-label fw-semibold"> Familiarity </label>
         <input type="range" class="form-range" id="customRange1" />
         <div class="d-flex justify-content-between">
-          <p>Low</p>
-          <p>Medium</p>
-          <p>High</p>
+          <p class="small-text text-start">Strongly disagree</p>
+          <p class="small-text text-center">Neither disagree nor agree</p>
+          <p class="small-text text-end">Strongly agree</p>
         </div>
         <label for="option4" class="form-label fw-semibold"> Timeliness </label>
         <input type="range" class="form-range" id="customRange1" />
         <div class="d-flex justify-content-between">
-          <p>Low</p>
-          <p>Medium</p>
-          <p>High</p>
+          <p class="small-text text-start">Strongly disagree</p>
+          <p class="small-text text-center">Neither disagree nor agree</p>
+          <p class="small-text text-end">Strongly agree</p>
         </div>
         <label for="option5" class="form-label fw-semibold"> Suggested by readers </label>
         <input type="range" class="form-range" id="customRange1" />
         <div class="d-flex justify-content-between">
-          <p>No</p>
-          <p>Maybe</p>
-          <p>Yes</p>
+          <p class="small-text text-start">Strongly disagree</p>
+          <p class="small-text text-center">Neither disagree nor agree</p>
+          <p class="small-text text-end">Strongly agree</p>
         </div>
-        <div class="form-check mt-2">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="idk" />
-          <label class="form-check-label fw-semibold" for="idk"> I don't know </label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="other" />
-          <label class="form-check-label fw-semibold" for="other"> Other </label>
+        <div>
+          <label class="form-label fw-semibold" for="other"> Other </label>
+          <textarea class="form-control" rows="3"></textarea>
         </div>
       </BaseModal>
     </BaseButton>
     <!-- <HelpButton help-text="Automatically identifies the claims made in this article." class="col" /> -->
   </div>
 </template>
+
+<style scoped>
+.small-text {
+  font-size: 12px;
+}
+</style>
