db8dbfe86af1cbbcc6db62001b55f5ad19cbb914
ananya <ananyasbhatt@gmail.com>
2024-10-10 01:31:28 +0100

    change text next to autocomplete table

--- src/components/AppAutocompleteButton.vue
+++ src/components/AppAutocompleteButton.vue
@@ -26,6 +26,6 @@ function autocompleteTable() {
       Autocomplete Table
     </BaseButton>
     <!-- <HelpButton help-text="Completes the rest of the table on a best effort basis." /> -->
-     <p class="col-7 fst-italic">Click to automatically assess the degree to which evidence corrorborates or casts doubt on claims</p>
+     <p class="col-7 fst-italic">Click on a cell to manually modify the assessment</p>
   </div>
 </template>
