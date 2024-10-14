08505d9a6eae38211eff2f3fd0193481b4c61c4a
ananya <ananyasbhatt@gmail.com>
2024-10-14 14:40:26 +0100

    add label underneath evidence tuner
    add a dynamic label under the evidence tuner slider and limit the colours to only 5 rather than having multiple shades

--- src/components/AchTableEvidenceTuner.vue
+++ src/components/AchTableEvidenceTuner.vue
@@ -1,27 +1,49 @@
 <script setup>
-import ThumbsUp from 'bootstrap-icons/icons/hand-thumbs-up-fill.svg'
-import ThumbsDown from 'bootstrap-icons/icons/hand-thumbs-down-fill.svg'
 import { useStore } from '@/store'
 import { storeToRefs } from 'pinia'
+import { ref } from 'vue'
 
 const store = useStore()
 
 const { evidenceTunerCellRef } = storeToRefs(store)
+const sliderLabel = ref(null)
+
+function updateLabel(event) {
+  switch (event.target.value) {
+    case '-1':
+      sliderLabel.value = 'Strongly Disagree'
+      break
+    case '-0.5':
+      sliderLabel.value = 'Disagree'
+      break
+    case '0':
+      sliderLabel.value = 'Neither disagree nor agree'
+      break
+    case '0.5':
+      sliderLabel.value = 'Agree'
+      break
+    case '1':
+      sliderLabel.value = 'Strongly Agree'
+      break
+    default:
+      sliderLabel.value = 'Error :('
+  }
+}
 </script>
 
 <template>
   <div class="d-flex flex-column">
     <input
+      @input="updateLabel"
       v-model="store.manualMatrix[evidenceTunerCellRef[1]][evidenceTunerCellRef[0]]"
       type="range"
       min="-1"
       max="1"
-      step="0.01"
+      step="0.5"
       class="form-range"
     />
-    <div class="d-flex justify-content-between">
-      <ThumbsDown />
-      <ThumbsUp />
-    </div>
+    <p>
+      {{ sliderLabel }}
+    </p>
   </div>
 </template>
