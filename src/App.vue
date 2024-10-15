f5a7d99449f57b058e994b9a3b144b2aad59201b
ananya <ananyasbhatt@gmail.com>
2024-10-15 04:57:22 +0100

    add help button
    this button leads to a page which will contain a tutorial on how to use this extension

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import UndoIcon from 'bootstrap-icons/icons/arrow-counterclockwise.svg'
 import RedoIcon from 'bootstrap-icons/icons/arrow-clockwise.svg'
 import SaveIcon from 'bootstrap-icons/icons/floppy.svg'
 import LoadIcon from 'bootstrap-icons/icons/folder.svg'
+import HelpIcon from 'bootstrap-icons/icons/info-lg.svg'
 
 const store = useStore()
 
@@ -35,10 +36,11 @@ async function tableDrop() {
 
     <main class="container-fluid flex-grow-1">
       <div class="d-flex justify-content-between mb-2">
-        <BaseButton @click="store.undo"><UndoIcon class="align-self-center" /> Undo</BaseButton>
+        <BaseButton @click="store.undo"><UndoIcon class="p-0 align-self-center" /> Undo</BaseButton>
         <BaseButton @click="store.redo"><RedoIcon /> Redo</BaseButton>
         <BaseButton @click="store.save"><SaveIcon /> Save</BaseButton>
         <BaseButton @click="store.load"><LoadIcon /> Load</BaseButton>
+        <a href="https://latif.arg.tech" target="_blank"><BaseButton><HelpIcon /> Help</BaseButton></a>
       </div>
 
       <div class="mb-2">
