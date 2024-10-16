9aab8a2accf16a12f73dc1987a7bde1797ecded2
Joshua Price <git@jprice.uk>
2024-10-16 09:16:41 +0100

    Add option to add tooltip to base button

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,9 +1,33 @@
 <script setup>
-defineProps({ loading: Boolean })
+import { onMounted, onUnmounted, ref } from 'vue'
+import * as bootstrap from 'bootstrap'
+
+defineProps({ loading: Boolean, tooltip: String })
+
+const tooltipObj = ref(null)
+const tooltipElement = ref(null)
+
+onMounted(() => {
+  tooltipObj.value = new bootstrap.Tooltip(tooltipElement.value)
+})
+
+onUnmounted(() => {
+  if (tooltipObj.value) {
+    tooltipObj.value.dispose()
+    tooltipObj.value = null
+  }
+})
 </script>
 
 <template>
-  <button type="button" :disabled="loading" class="btn btn-primary">
+  <button
+    ref="tooltipElement"
+    type="button"
+    :disabled="loading"
+    class="btn btn-primary"
+    data-bs-toggle="tooltip"
+    :data-bs-title="tooltip"
+  >
     <slot />
     <span v-if="loading" aria-hidden="true" class="spinner-border spinner-border-sm ms-2"></span>
   </button>
