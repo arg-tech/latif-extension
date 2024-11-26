d3833661b32d74903df089585825d778ead2c4a9
Joshua Price <git@jprice.uk>
2024-11-26 15:03:04 +0000

    Move taskbar into separate component

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,7 @@ import DraftReportButton from '@/components/AppDraftReportButton.vue'
 // import HelpButton from '@/components/AppHelpButton.vue'
 import { useStore } from '@/store'
 import { doUrlsMatch, getCurrentTab } from '@/utils'
-import BaseButton from '@/components/BaseButton.vue'
-import UndoIcon from 'bootstrap-icons/icons/arrow-counterclockwise.svg'
-import RedoIcon from 'bootstrap-icons/icons/arrow-clockwise.svg'
-import SaveIcon from 'bootstrap-icons/icons/floppy.svg'
-import LoadIcon from 'bootstrap-icons/icons/folder.svg'
-import HelpIcon from 'bootstrap-icons/icons/info-lg.svg'
+import Taskbar from '@/components/AppTaskbar.vue'
 
 const store = useStore()
 
@@ -35,12 +30,9 @@ async function tableDrop() {
     <BaseHeader class="mt-2 mb-4 container-fluid" />
 
     <main class="container-fluid flex-grow-1">
-      <div class="d-flex gap-1 mb-2">
-        <BaseButton @click="store.undo"><UndoIcon class="p-0 align-self-center" /> Undo</BaseButton>
-        <BaseButton @click="store.redo"><RedoIcon /> Redo</BaseButton>
-        <BaseButton @click="store.save" tooltip="Save the table to a file"><SaveIcon /> Save</BaseButton>
-        <BaseButton @click="store.load" tooltip="Load a previously saved file"><LoadIcon /> Load</BaseButton>
-        <a href="https://latif.arg.tech" target="_blank"><BaseButton><HelpIcon /> Help</BaseButton></a>
+
+      <div class="mb-2">
+        <Taskbar />
       </div>
 
       <div class="mb-2">
---
+++ src/components/AppTaskbar.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { useStore } from '@/store'
+import BaseButton from '@/components/BaseButton.vue'
+import UndoIcon from 'bootstrap-icons/icons/arrow-counterclockwise.svg'
+import RedoIcon from 'bootstrap-icons/icons/arrow-clockwise.svg'
+import SaveIcon from 'bootstrap-icons/icons/floppy.svg'
+import LoadIcon from 'bootstrap-icons/icons/folder.svg'
+import HelpIcon from 'bootstrap-icons/icons/info-lg.svg'
+
+const store = useStore()
+</script>
+
+<template>
+  <div class="d-flex gap-1">
+    <BaseButton @click="store.undo"><UndoIcon class="p-0 align-self-center" />Undo</BaseButton>
+    <BaseButton @click="store.redo"><RedoIcon />Redo</BaseButton>
+    <BaseButton @click="store.save" tooltip="Save the table to a file">
+      <SaveIcon />Save
+    </BaseButton>
+    <BaseButton @click="store.load" tooltip="Load a previously saved file">
+      <LoadIcon />Load
+    </BaseButton>
+    <a href="https://latif.arg.tech" target="_blank">
+      <BaseButton><HelpIcon /> Help</BaseButton>
+    </a>
+  </div>
+</template>
